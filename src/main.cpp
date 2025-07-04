eb79fd0a33c7ce330cc4e036093f4fb1b834636a
Loic Tournier <loic.tournier@gmail.com>
2025-07-04 11:25:59 +0200

    add blinking led + wifi password

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@
 #include <ESPAsyncWebServer.h>
 
 // Set password to "" for open networks.
-const char* ssid = "Freebox-2074E2";
-const char* pass = "tv733q46tfq7cz64qdw47z";
+//const char* ssid = "Freebox-2074E2";
+//const char* pass = "tv733q46tfq7cz64qdw47z";
+const char* ssid = "lolobotlab_xiaomi";
+const char* pass = "eristofly01";
 
 //Server
 AsyncWebServer server(80);
 // Set your Static IP address
-IPAddress local_IP(192, 168, 1, 200);
+IPAddress local_IP(192, 168, 1, 104);
 // Set your Gateway IP address
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 0, 0);
@@ -36,6 +38,12 @@ void setPinsSequentially(const int* pins, int numPins) {
         delay(1000); // Wait 1 second
     }
 }
+void blinkRedLed() {
+    digitalWrite(pinredLed, LOW);
+    delay(100);
+    digitalWrite(pinredLed, HIGH);
+    delay(100);
+}
 
 void startrelais(const int* pins, int pin_nb){
   digitalWrite(pins[pin_nb-1], HIGH);
@@ -50,17 +58,20 @@ void stoprelais(const int* pins, int pin_nb){
 }
 
 void handleStart(AsyncWebServerRequest *request) {
+  
     if (request->hasParam("relay")) {
         String paramValue = request->getParam("relay")->value();
         if (paramValue=="all"){
           for (int i=1; i<5;i++){
             startrelais(relayPins, i);
+            blinkRedLed();
           }
           request->send(200, "text/plain", "All relays started");
         }
         else{
           int relayIndex = paramValue.toInt();
           startrelais(relayPins, relayIndex);
+          blinkRedLed();
           request->send(200, "text/plain", "Relay started: " + String(relayIndex));
         }
 
@@ -70,17 +81,20 @@ void handleStart(AsyncWebServerRequest *request) {
 }
 
 void handleStop(AsyncWebServerRequest *request) {
+  
     if (request->hasParam("relay")) {
         String paramValue = request->getParam("relay")->value();
         if (paramValue=="all"){
           for (int i=1; i<5;i++){
             stoprelais(relayPins, i);
+            blinkRedLed();
           }
           request->send(200, "text/plain", "All relays stoped");
         }
         else{
           int relayIndex = paramValue.toInt();
           stoprelais(relayPins, relayIndex);
+          blinkRedLed();
           request->send(200, "text/plain", "Relay stoped: " + String(relayIndex));
         }
 
@@ -90,14 +104,18 @@ void handleStop(AsyncWebServerRequest *request) {
 }
 
 
+
 void setup() {
 
+  //Switch red led on
+  pinMode(pinredLed, OUTPUT);
+  digitalWrite(pinredLed, HIGH);
+  blinkRedLed();
   //--------Serial comm begin-----------
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
   Serial.println("");
-
   //--------Wifi begin------------------
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -112,12 +130,16 @@ void setup() {
   // Configures static IP address
   if (!WiFi.config(local_IP, gateway, subnet)) {
     Serial.println("STA Failed to configure");
+    blinkRedLed();
+    blinkRedLed();
   }
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
+  blinkRedLed();
   //--------Set all to Low-------------
   for (int i = 0; i < 4; i++) {
       pinMode(relayPins[i], OUTPUT);
+      blinkRedLed();
       digitalWrite(relayPins[i], LOW); // Start with all OFF
   }
 
